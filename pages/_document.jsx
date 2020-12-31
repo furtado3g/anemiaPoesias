cb94358693559ba7dbe95206e60f4afb1c916afb
Lucas Furtado <lucas_shiguioka@hotmail.com>
2020-12-31 16:08:47 -0300

    .

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,14 +1,14 @@
-import Document, { Head, Main, NextScript } from "next/document";
+import Document, { Head, Main, NextScript } from 'next/document';
 // Import styled components ServerStyleSheet
-import { ServerStyleSheet } from "styled-components";
-import {Body}  from '../components/body'
-class MyDocument extends Document {
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     // Step 1: Create an instance of ServerStyleSheet
     const sheet = new ServerStyleSheet();
     // Step 2: Retrieve styles from components in the page
     const page = renderPage((App) => (props) =>
-      sheet.collectStyles(<App {...props} />)
+      sheet.collectStyles(<App {...props} />),
     );
     // Step 3: Extract the styles as <style> tags
     const styleTags = sheet.getStyleElement();
@@ -18,22 +18,17 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <html lang="pt-br">
+      <html>
         <Head>
-          <title>Anemia Poesias</title>
-          <meta name="mobile-web-app-capable" content="yes"></meta>
+          <title>My app</title>
           {/* Step 5: Output the styles in the head  */}
           {this.props.styleTags}
         </Head>
-        <body >
-          <Body>
-            <Main />
-            <NextScript />
-          </Body>
-          <script src="https://kit.fontawesome.com/c4c1b5ac48.js" crossorigin="anonymous"></script>
+        <body>
+          <Main />
+          <NextScript />
         </body>
       </html>
     );
   }
 }
-export default  MyDocument
