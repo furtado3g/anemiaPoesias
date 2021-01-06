a6764c2d80b67435ab099b96fca95e8235457b8b
Lucas Furtado <lucas_shiguioka@hotmail.com>
2021-01-06 12:32:18 -0300

    iniciado o desenvolvimento da tela de registro de poesias iniciado o desenvolvimento do estilo do mesmo

---
+++ components/poetry/new/style.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Page = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  margin-top: 7.5vh;
+  width: 60%;
+  min-height: 60%;
+  border-radius: 5px;
+  background-color: #f2f2f2;
+`;
+
+export { Page, Container };
---
+++ pages/poetry/new.tsx
@@ -0,0 +1,15 @@
+import { Body } from "../../components/body";
+import { Page, Container } from "../../components/poetry/new/style";
+const New = () => {
+  return (
+    <Body>
+      <Page>
+        <Container>
+          <h1>Ola Mundo</h1>
+        </Container>
+      </Page>
+    </Body>
+  );
+};
+
+export default New;
